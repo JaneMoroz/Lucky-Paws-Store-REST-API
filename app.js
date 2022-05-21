910378b5ee68cb5a5f0b5be9a9fb4c5722376e09
Jane Moroz <Moroz.jane@yahoo.com>
2022-05-21 15:35:26 +0300

    Added Some Security

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ const express = require('express');
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
 
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const cors = require('cors');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
+
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 
@@ -16,6 +23,9 @@ const orderController = require('./controllers/orderController');
 
 const app = express();
 
+// Trust proxies
+app.enable('trust proxy');
+
 // Setting PUG view engine
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
@@ -26,10 +36,54 @@ app.set('views', path.join(__dirname, 'views'));
 // Serving static files
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Set Security HTTP Headers
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'", 'https:', 'http:', 'data:', 'ws:', 'blob:'],
+      baseUri: ["'self'"],
+      fontSrc: ["'self'", 'https:', 'http:', 'data:'],
+      scriptSrc: [
+        "'self'",
+        'https:',
+        'http:',
+        'blob:',
+        'https://*.mapbox.com',
+        ,
+        'https://*.stripe.com',
+        'data:',
+        'ws:',
+      ],
+      styleSrc: ["'self'", "'unsafe-inline'", 'https:', 'http:'],
+      objectSrc: ["'none'"],
+      workerSrc: ["'self'", 'data:', 'blob:'],
+      childSrc: ["'self'", 'https:', 'blob:'],
+      imgSrc: ["'self'", 'data:', 'blob:'],
+      connectSrc: ["'self'", 'blob:', 'https:', 'ws:'],
+    },
+  })
+);
+
+// Implement Cors
+app.use(
+  cors({
+    origin: '/',
+    credentials: true,
+  })
+);
+
 // Development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+// Exmple of requests limiter
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000, // 100 request from one IP in one hour
+  message: 'Too many requests from this IP, please try again in an hour!',
+});
+app.use('/api', limiter);
 
 // Stripe Webhook
 // app.post(
@@ -47,11 +101,11 @@ app.use(express.urlencoded({ extended: true, limit: '10kb' }));
 // To parse data from cookies
 app.use(cookieParser());
 
-// Test middleware
-app.use((req, res, next) => {
-  req.requestTime = new Date().toISOString();
-  next();
-});
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
 
 ////////////////////////////////////////////////////////////////
 // Routes
